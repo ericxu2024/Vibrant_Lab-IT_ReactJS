44b178c7824371e4fb16e2e01d41e7aa4611daa4
Eric Xu <ericxu2024@berkeley.edu>
2024-03-19 16:57:36 -0700

    have a tabel with working search bar, used state management for the search text

---
+++ dynamic-web-app/src/components/TableContent.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './TableContent.css'; 
+
+const TableContent = ({ data }) => {
+  return (
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>position</th>
+            <th>department</th>
+            <th>age</th>
+            <th>salary</th>
+            <th>experience</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.position}</td>
+              <td>{employee.department}</td>
+              <td>{employee.age}</td>
+              <td>{employee.salary}</td>
+              <td>{employee.experience}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableContent;
